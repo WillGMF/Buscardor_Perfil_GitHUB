cc324ce04f7ea3b5ad020900d3024e786436637b
Willian Gabriel <willian.minikovski@hotmail.com>
2023-10-04 16:13:56 -0300

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import Perfil from "./components/Perfil/Perfil"
+import { useState } from "react"
+
+import Perfil from "./components/Perfil"
+import Formulario from "./components/Formulario"
+import ReposList from "./components/ReposList";
 
 
 
 function App() {
+  const [FormularioEstaVisivel, setFormularioEstaVisivel] =
+  useState(true);
+  const [nomeUsuario, setNomeUsuario ] = useState("")
+
   return (
     <>
-      <Perfil />
+    <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)}></input>
+
+    {nomeUsuario.length > 4 && (
+      <>
+        <Perfil nomeUsuario={nomeUsuario} />
+        <ReposList nomeUsuario={nomeUsuario}/>
+      </>
+    )}
+
+
+
+
+
+      {/* {FormularioEstaVisivel && (
+        <Formulario />
+      )} */}
+      {/* <button onClick={() => setFormularioEstaVisivel(!FormularioEstaVisivel)} type="button">Formulario</button> */}
     </>
   )
 }
