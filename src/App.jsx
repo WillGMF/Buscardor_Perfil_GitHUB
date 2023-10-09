22d0a5526988f1a2af142341c976584dda21ad16
Willian Gabriel <willian.minikovski@hotmail.com>
2023-10-09 11:40:44 -0300

    sim

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,15 @@
 import { useState } from "react"
-
-import Perfil from "./components/Perfil"
 import Formulario from "./components/Formulario"
-import ReposList from "./components/ReposList";
+
 
 
 
 function App() {
-  const [FormularioEstaVisivel, setFormularioEstaVisivel] =
-  useState(true);
-  const [nomeUsuario, setNomeUsuario ] = useState("")
-
   return (
     <>
-    <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)}></input>
-
-    {nomeUsuario.length > 4 && (
-      <>
-        <Perfil nomeUsuario={nomeUsuario} />
-        <ReposList nomeUsuario={nomeUsuario}/>
-      </>
-    )}
-
-
-
-
-
-      {/* {FormularioEstaVisivel && (
-        <Formulario />
-      )} */}
-      {/* <button onClick={() => setFormularioEstaVisivel(!FormularioEstaVisivel)} type="button">Formulario</button> */}
+    <Formulario/>
     </>
+    
   )
 }
 
--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,63 +1,63 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
+
 
 const Formulario = () => {
-    const [materiaA, setMateriaA] = useState(0);
-    const [materiaB, setMateriaB] = useState(0);
-    const [materiaC, setMateriaC] = useState(0);
-    const [nome, setNome] = useState('');
-
-    useEffect(() => {
-        console.log("O componente iniciou");
-
-        return () => {
-            console.log("o componente desmontou");
-        }
-    }, []) 
+    const [peso, setPeso] = useState(0)
+    const [altura, setAltura] = useState(0)
     
-
     
-    useEffect(() => {
-        console.log("lala");
-    }, [nome])
-    
-    const alteraNome = (evento) => {
-        setNome(estadoAnterior => {
-            return evento.target.value
-        })
-    }
-
     const renderizaResultado = () => {
-        const soma = materiaA + materiaB + materiaC;
-        const media = soma / 3;
+        const alturaCalculo = altura * altura;
+        const imc = (peso / alturaCalculo).toFixed(2);
+        
 
-        if (media >= 7) {
+        if (imc <= 18.5) {
             return (
-                <p>
-                    Olá {nome}, foi aprovado
-                </p>
+                <h4>Seu IMC é de: {imc} <br /> 
+                E sua classificação na tabela é de: Baixo peso</h4>
             )
-        } else {
+        } else if (imc >= 18.5 && imc <= 24.99) {
             return (
-                <p>
-                    Olá {nome}, foi reprovado
-                </p>
+                <h4>Seu IMC é de: {imc} <br /> 
+                E sua classificação na tabela é de: Médio peso</h4>
+            )
+        } else if (imc >= 25 && imc <= 29.99) {
+            return (
+                <h4>Seu IMC é de: {imc} <br /> 
+                E sua classificação na tabela é de: Sobrepeso</h4>
+            )
+        } else if (imc >= 30 && imc <= 34.99) {
+            return (
+                <h4>Seu IMC é de: {imc} <br /> 
+                E sua classificação na tabela é de: Obesidade grau I</h4>
+            )
+        } else if (imc >= 35 && imc <= 39.99) {
+            return (
+                <h4>Seu IMC é de: {imc} <br /> 
+                E sua classificação na tabela é de: Obesidade grau II</h4>
+            )
+        } else if (imc >= 40) {
+            return (
+                <h4>Seu IMC é de: {imc} <br /> 
+                E sua classificação na tabela é de: Obesidade grau II</h4>
             )
         }
-    }
+        
+        }
+    
 
     return (
-        <form>
-            <ul>
-                {[1, 2, 3, 4, 5].map(item => (
-                    <li key={item}>{item}</li>)
-                )}
-            </ul>
-            <input type="text" placeholder="Seu nome" onChange={alteraNome} />
-            <input type="number" placeholder="Nota matéria A" onChange={({ target }) => setMateriaA(parseInt(target.value))}/>
-            <input type="number" placeholder="Nota matéria B" onChange={({ target }) => setMateriaB(parseInt(target.value))}/>
-            <input type="number" placeholder="Nota matéria C" onChange={({ target }) => setMateriaC(parseInt(target.value))}/>
-            {renderizaResultado()}
+        <div className="container">
+            <form>
+            <div className="divAltura">
+            Sua altura: <input type="number" placeholder="Exemplo : 1.80" onChange={({target}) => setAltura(parseFloat(target.value))}/> Metros
+            </div>
+            <div>
+            Seu peso: <input type="number" placeholder="Exemplo 55" onChange={({target}) => setPeso(parseFloat(target.value))}/> KG
+            </div>
+            <h3>{renderizaResultado()} </h3>
         </form>
+        </div>
     )
 }
 
